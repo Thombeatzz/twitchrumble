f2756a17aa2c66a58e9ae60cd40f6dfdcb74249c
Thombeatzz <thombeatzz@gmail.com>
2025-02-14 23:51:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get("/", (req, res) => {
 app.get("/sendChat", (req, res) => {
     let message = req.query.message || "Fehlende Nachricht";
     console.log("Empfangene Nachricht:", message);
-    res.send("OK");
+    res.send(message);
 });
 
 app.post("/sendChat", (req, res) => {
